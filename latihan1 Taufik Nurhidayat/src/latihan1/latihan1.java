37f80176ef434195402ac79dd46ea421fb7cc965
TaufikNurhidayat74 <topekajh4@gmail.com>
2026-04-02 13:10:06 +0700

    Add files via upload

---
+++ latihan1 Taufik Nurhidayat/src/latihan1/latihan1.java
@@ -0,0 +1,8 @@
+package src.latihan1;
+
+public class latihan1 {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+        System.out.println("belajar kode sederhana");
+    }
+}
